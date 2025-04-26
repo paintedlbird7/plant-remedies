e32c46fef164e95d59ca9098c48bb14cc46a309a
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-26 10:14:38 -0700

    wip: using postgres to get dog data

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    ailment = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    origin = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 # main_app/views.py
 from django.shortcuts import render
+from .models import Plant
 
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
@@ -13,24 +14,8 @@ def about(request):
     # return HttpResponse('<h1>About the PlantCollector</h1>')
     return render(request, 'about.html')
 
-class Plant:
-    def __init__(self, name, ailment, description, origin, image):
-        self.name = name
-        self.ailment = ailment
-        self.description = description
-        self.origin = origin
-        self.image = image
-
-
-
-# Create a list of Plant instances
-plants = [
-    Plant('Manzanilla', 'Stomach Ache', 'Chamomile tea is often used for digestion and calming the stomach.', 'Mexico', 'chamomile.png'),
-    # Plant('Eucalipto', 'Cough', 'Eucalyptus leaves are boiled for clearing the lungs and soothing coughs.', 'South America'),
-    Plant('Hierba Buena', 'Nausea', 'Mint is steeped into tea to relieve nausea and promote digestion.', 'Mexico', 'mint.png'),
-    # Plant('Ruda', 'Menstrual Pain', 'Rue is used traditionally to ease menstrual discomfort.', 'Latin America')
-]
-
 def plant_index(request):
-    # Render the plants/index.html template with the plants data
+    plants = Plant.objects.all()
     return render(request, 'plants/index.html', {'plants': plants})
+
+# my db I created is called 'plantcollector2'
