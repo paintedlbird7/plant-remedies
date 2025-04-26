1cd72ddc6795a6da8487e55c50bb117f8d7b20e5
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-26 08:58:51 -0700

    displays an index page with a dynamically rendered list of Plant objects

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    # route for plants index
+    path('plants/', views.plant_index, name='plant-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -12,3 +12,25 @@ def home(request):
 def about(request):
     # return HttpResponse('<h1>About the PlantCollector</h1>')
     return render(request, 'about.html')
+
+class Plant:
+    def __init__(self, name, ailment, description, origin, image):
+        self.name = name
+        self.ailment = ailment
+        self.description = description
+        self.origin = origin
+        self.image = image
+
+
+
+# Create a list of Plant instances
+plants = [
+    Plant('Manzanilla', 'Stomach Ache', 'Chamomile tea is often used for digestion and calming the stomach.', 'Mexico', 'chamomile.png'),
+    # Plant('Eucalipto', 'Cough', 'Eucalyptus leaves are boiled for clearing the lungs and soothing coughs.', 'South America'),
+    Plant('Hierba Buena', 'Nausea', 'Mint is steeped into tea to relieve nausea and promote digestion.', 'Mexico', 'mint.png'),
+    # Plant('Ruda', 'Menstrual Pain', 'Rue is used traditionally to ease menstrual discomfort.', 'Latin America')
+]
+
+def plant_index(request):
+    # Render the plants/index.html template with the plants data
+    return render(request, 'plants/index.html', {'plants': plants})
