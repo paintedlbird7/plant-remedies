f4d1c6d4f5f86cd25873ede059a13c03fac9639c
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-05-06 18:53:56 -0700

    Get ready for production2

--- plant2collector2/settings.py
+++ plant2collector2/settings.py
@@ -21,7 +21,6 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-WSGI_APPLICATION
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -31,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not 'ON_HEROKU' in os.environ:
-DEBUG = True
+    DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
